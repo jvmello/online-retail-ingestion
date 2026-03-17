fc79e04c079fa89a7deea6de9f505c2cc79a01dd
jvmello <jmachadodemello@gmail.com>
2026-03-16 22:01:04 -0300

    Update code from transformation and job folders #environment [enhancement]

--- jobs/ingest_raw.py
+++ jobs/ingest_raw.py
@@ -1,15 +1,19 @@
 from jobs.spark_session import create_spark_session
+from jobs.constants import LOCAL_ONLINE_RETAIL_CSV, RAW_ONLINE_RETAIL
 
-from jobs.constants import LOCAL_ONLINE_RETAIL_CSV
 
-spark = create_spark_session("gold")
+def main() -> None:
+    spark = create_spark_session("ingest-raw-online-retail")
 
-df = spark.read.csv(
-    LOCAL_ONLINE_RETAIL_CSV,
-    header=True,
-    inferSchema=True
-)
+    df = spark.read.csv(
+        LOCAL_ONLINE_RETAIL_CSV,
+        header=True,
+        inferSchema=True,
+    )
 
-df.write \
-  .mode("overwrite") \
-  .parquet("s3a://lakehouse/raw/online_retail/")
+    df.write.mode("overwrite").parquet(RAW_ONLINE_RETAIL)
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
--- jobs/silver.py
+++ jobs/silver.py
@@ -1,19 +1,17 @@
 from jobs.spark_session import create_spark_session
+from jobs.constants import RAW_ONLINE_RETAIL, SILVER_ONLINE_RETAIL
+from transformations.silver import build_silver_online_retail
 
-from pyspark.sql.functions import col, to_timestamp
 
-spark = create_spark_session("silver")
+def main() -> None:
+    spark = create_spark_session("build-silver-online-retail")
 
-df_raw = spark.read.parquet("s3a://lakehouse/raw/online_retail/")
+    df_raw = spark.read.parquet(RAW_ONLINE_RETAIL)
+    df_silver = build_silver_online_retail(df_raw)
 
-df_silver = (
-    df_raw
-    .withColumn("InvoiceDate", to_timestamp(col("InvoiceDate")))
-    .withColumn("Quantity", col("Quantity").cast("int"))
-    .withColumn("UnitPrice", col("UnitPrice").cast("double"))
-    .filter(col("Quantity").isNotNull())
-    .filter(col("UnitPrice").isNotNull())
-    .withColumn("Revenue", col("Quantity") * col("UnitPrice"))
-)
+    df_silver.write.mode("overwrite").parquet(SILVER_ONLINE_RETAIL)
+    spark.stop()
 
-df_silver.write.mode("overwrite").parquet("s3a://lakehouse/silver/online_retail/")
+
+if __name__ == "__main__":
+    main()
